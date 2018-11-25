967c9c352a78d9370ee83b440ab9088d60abfa2b
Omair Anwar <omairanwar@gmail.com>
2018-11-25 14:30:38 -0500

    added PO create functionality, still working on the parsing to determine file number, but this is the structure of the data there's a sample xml file here too with hardcoded values from engine for now(of course this will change based on data)

---
+++ B2C/src/controler/Order.java
@@ -0,0 +1,50 @@
+package controler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Engine;
+
+/**
+ * Servlet implementation class Order
+ */
+@WebServlet("/Order.do")
+public class Order extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Order() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Engine engine = Engine.getInstance();
+		try {
+			engine.createOrders();
+			
+		}
+		catch(Exception e){System.out.println(e.getMessage()); }
+		//this.getServletContext().getRequestDispatcher("/pages/Orders.jspx").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- B2C/src/model/Engine.java
+++ B2C/src/model/Engine.java
@@ -1,69 +1,129 @@
 package model;
 
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import Orders.CustomerType;
+import Orders.ItemType;
+import Orders.ItemsType;
+import Orders.ObjectFactory;
+import Orders.OrderType;
 
 
 public class Engine {
-	private static Engine instance = null;	
-	
-	private Engine()
-	{
-		
+	private final String poPath = "/home/user/git/FoodsRUs/B2C/Orders/";
+	private static Engine instance = null;
+	private static ObjectFactory factory = new ObjectFactory();
+	private Engine() {
+
 	}
 
 	/**
 	 * @Description creates and delivers a singleton instance
-	 * @return returns the singleton Engine object*/
-	public synchronized static Engine getInstance()
-	{
-		if (instance == null) instance = new Engine();
+	 * @return returns the singleton Engine object
+	 */
+	public synchronized static Engine getInstance() {
+		if (instance == null)
+			instance = new Engine();
 		return instance;
 	}
-	
+
 	/**
-	 * @Description It takes in a search term and searches the database through the ItemDAO
+	 * @Description It takes in a search term and searches the database through the
+	 *              ItemDAO
 	 * @param searchTerm
 	 * @return a List<ItemBean> containing all items that match the search term
-	 * @throws Exception 
-	 * */
-	public List<ItemBean> doItem(String searchTerm, String flag) throws Exception
-	{
-		/*split the search term by using regex
-		 * loopthrough the array and pass a single search term to the DAO
-		 * get the list from the DAO and add the contents to the total list
-		 * return the total list*/
-		/*if flag is not null, then the search bar is used with multiple search words*/
+	 * @throws Exception
+	 */
+	public List<ItemBean> doItem(String searchTerm, String flag) throws Exception {
+		/*
+		 * split the search term by using regex loopthrough the array and pass a single
+		 * search term to the DAO get the list from the DAO and add the contents to the
+		 * total list return the total list
+		 */
+		/*
+		 * if flag is not null, then the search bar is used with multiple search words
+		 */
 		String bySearchTerm = "bySearchTerm";
-		if(bySearchTerm.equals(flag)) {
+		if (bySearchTerm.equals(flag)) {
 			String split[] = searchTerm.split("[\\s+]|[,+]");
 			ArrayList<String> terms = new ArrayList<>();
 			for (String term : split) {
-				if(!term.isEmpty()) {
-					terms.add(term);				
+				if (!term.isEmpty()) {
+					terms.add(term);
 				}
 			}
 			return ItemDAO.retrieve(terms);
 		} else {
-			/*if flag is null, then it is being searched by a specific identifier*/
-			/*parse the catID to int and pass it to the DAO*/
+			/* if flag is null, then it is being searched by a specific identifier */
+			/* parse the catID to int and pass it to the DAO */
 			int catid = -1;
 			try {
 				catid = Integer.parseInt(searchTerm);
 			} catch (Exception e) {
 				throw new Exception("internal problem catID is not parsed to integer");
 			}
-			
+
 			return ItemDAO.retrieve(catid);
 		}
-			
+
+	}
+
+	public void createOrders() throws DatatypeConfigurationException, JAXBException, IOException {
+		
+		CustomerType customer = factory.createCustomerType();
+		
+		ItemType item = factory.createItemType();
+		ItemsType items = factory.createItemsType();
+		OrderType order = factory.createOrderType();
+		BigDecimal totalPrice;
+		customer.setName("Amin");
+		customer.setAccount("new account");
+		item.setName("chicken");
+		item.setNumber("34");
+		item.setPrice(new BigDecimal(12.3));
+		totalPrice = item.getPrice(); 
+		item.setQuantity(BigInteger.valueOf( new Integer(3).intValue()));
+		List<ItemType> itemsList = items.getItem();
+		itemsList.add(item);
+		item.setName("cheese");
+		item.setNumber("35");
+		item.setPrice(new BigDecimal(12));
+		item.setQuantity(BigInteger.valueOf( new Integer(12).intValue()));
+		itemsList.add(item);
+		totalPrice.add(item.getPrice());
+		order.setCustomer(customer);
+		order.setItems(items);
+		order.setTotal(totalPrice);
+		order.setShipping(new BigDecimal(12));
+		order.setHST(order.getGrandTotal().multiply(new BigDecimal(0.13)));
+		order.setGrandTotal(totalPrice.add(order.getHST().add(order.getShipping())));
+		order.setId(BigInteger.valueOf(new Integer(321).intValue()));
+		GregorianCalendar c = new GregorianCalendar();
+		XMLGregorianCalendar date2 = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+		order.setSubmitted(date2);
+		JAXBContext jc = JAXBContext.newInstance(order.getClass());
+		Marshaller marsh = jc.createMarshaller();
+		marsh.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		marsh.marshal(order, new File(poPath+customer.getName()+"_"+order.getId().toString()+".xml"));
+
 		
 	}
-	
-	
-	public List<CategoryBean> doCategory(String prefix) throws Exception
-	{
+
+	public List<CategoryBean> doCategory(String prefix) throws Exception {
 		return CategoryDAO.retrieve(prefix);
 	}
-	
+
 }
